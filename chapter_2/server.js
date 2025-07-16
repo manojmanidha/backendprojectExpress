cb76b506ab617012ba9e531758d658a9dc44cf6f
Manoj K S <manojks.062001@gmail.com>
2025-07-16 13:27:39 +0530

    basic backend http crud operation

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -5,9 +5,9 @@ const express = require('express')
 const app = express();
 const PORT = 1235;
 
-const data = {
-    "name":"kishor"
-}
+const data = ["kishor"]
+
+app.use(express.json())
 
 // type-1 --> website endpoint(when the url is entered in the browser this send the html page)
 
@@ -16,12 +16,17 @@ app.get('/' , (req , res)=>{
     res.send(`<body>
         <h1>Dataa :</h1>
         <p>${JSON.stringify(data)}</p>
+        <a href="/dashboard">Dashboard</a>
         </body>`)
 })
 
 app.get('/dashboard' , (req , res) => {
     console.log('hit the dashboard route')
-    res.send('hi')
+    res.send(`<body>
+        <h1>dashboard :</h1>
+        <p>${JSON.stringify(data)}</p>
+        <a href="/">Home</a>
+        </body>`)
 })
 
 // CRUD --> c-POST , R-GET , U--> PUT , D--> DELET
@@ -30,6 +35,19 @@ app.get('/dashboard' , (req , res) => {
 app.get('/api/data' , (req , res) => {
     console.log('api response data')
     res.send(data)
+})
+
+app.post('/api/data' , (req , res) => {
+    const Entry = req.body
+    console.log('testing api', Entry)
+    data.push(Entry.name)
+    res.sendStatus(201)
+})
+
+app.delete('/api/endpoint' , (req , res) => {
+    data.pop()
+    console.log('remove the existing data from the array')
+    res.sendStatus(203)
 })
 
 app.listen(PORT , ()=>{console.log(`Server started running on port : ${PORT}`)});
