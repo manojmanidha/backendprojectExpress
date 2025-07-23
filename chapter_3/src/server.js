8f2ba08ee80be67e3467dd822f1201cf3568680b
Manoj K S <manojks.062001@gmail.com>
2025-07-23 16:07:31 +0530

    send website from server

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import path , {dirname} from 'path';
+import {fileURLToPath} from 'url';
 
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 
+
+app.get('/' , ()=>{
+    res.sendFile(path)
+})
+
+app.listen(PORT , ()=>{
+    console.log('server started at port :' , PORT)
+})
