e7ca68b55528a4d544d757b73ebb3e3b21b6368b
Manoj K S <manojks.062001@gmail.com>
2025-07-25 11:34:18 +0530

    database setup for creating table user , todo list when server boots

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -3,10 +3,18 @@ import path , {dirname} from 'path';
 import {fileURLToPath} from 'url';
 
 const app = express();
-const PORT = process.env.PORT || 
+const PORT = process.env.PORT || 5000
+
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = dirname(__filename)
+
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname , '../public')))
 
 app.get('/' , ()=>{
-    res.sendFile(path)
+    res.sendFile(path.join(__dirname , 'public' , 'index.html'))
 })
 
 app.listen(PORT , ()=>{
