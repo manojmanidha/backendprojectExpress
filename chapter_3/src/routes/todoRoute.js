66115fb828c1a91b754654b1f5b09ae22fc86f0b
manojmanidha <manojks.062001@gmail.com>
2025-07-31 15:35:38 +0530

    bug fix

--- chapter_3/src/routes/todoRoute.js
+++ chapter_3/src/routes/todoRoute.js
@@ -9,7 +9,7 @@ router.get('/' , (req , res)=>{})
 // to create new todo
 router.post('/' , (req , res)=>{})
 
-// to update the todo list using id of perticuler todo
+// to update the todo list using id of todo
 router.put('/:id' , (req , res)=>{})
 
 export default router
