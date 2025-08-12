c9a9fac068148bc42636fe2fbff8b355febea80a
manojmanidha <manojks.062001@gmail.com>
2025-08-12 18:38:44 +0530

    todoroute changes

--- chapter_3/src/routes/todoRoute.js
+++ chapter_3/src/routes/todoRoute.js
@@ -9,10 +9,14 @@ router.get('/' , (req , res)=>{
 })
 
 // to create new todo
-router.post('/' , (req , res)=>{})
+router.post('/' , (req , res)=>{
+    console.log('req' , req.body)
+})
 
 // to update the todo list using id of todo
-router.put('/:id' , (req , res)=>{})
+router.put('/:id' , (req , res)=>{
+    console.log('req' , req.body)
+})
 
 // to delete todo
 router.delete('/:id' , (req,res)=>{})
